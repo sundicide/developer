878ebbf38e23b242aca933594a59808a5fd7b789
guegit <ctbcban@gmail.com>
2018-04-23 16:10:23 +0900

    EditCode 싱글 쿼츠로 변경 및 새로운 상수 추가

--- vuejs/HW/03.Todo/src/constant.js
+++ vuejs/HW/03.Todo/src/constant.js
@@ -1,10 +1,11 @@
 export default {
-  FETCH_TODOLIST : "fetchTodolist",
-  ADD_TODOLIST: "addTodolist",
-  DELETE_TODOLIST: "deleteTodolist",
+  FETCH_TODOLIST : 'fetchTodolist',
+  ADD_TODOLIST: 'addTodolist',
+  DELETE_TODOLIST: 'deleteTodolist',
   FILTER_MODE : {
     ALL: 'showAllTodo',
     REMAIN: 'showRemainTodo',
     COMPLETE: 'showCompleteTodo',
   },
+  CHANGE_FILTER_MODE: 'changeFilterMode',
 }
