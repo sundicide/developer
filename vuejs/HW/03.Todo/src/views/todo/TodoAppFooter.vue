53e6bc588517df2300c0e4a056ea76c29e9b085d
guegit <ctbcban@gmail.com>
2018-04-23 10:14:57 +0900

    AddCode 필터 버튼에는 무조건 선택 효과가 보이도록 처리
    CSS를 넣었다 뺏다 하는 방식으로 구현

--- vuejs/HW/03.Todo/src/views/todo/TodoAppFooter.vue
+++ vuejs/HW/03.Todo/src/views/todo/TodoAppFooter.vue
@@ -1,11 +1,11 @@
 <template>
   <div :class="$style.div">
     <el-button-group>
-      <el-button type="primary" plain v-for="(value, index) in filtermode" :key="index" @click.natvie="filterButtonClick(value)">
+      <el-button type="primary" v-for="(value, index) in filtermode" :key="index" @click.natvie="filterButtonClick(value, index)" ref="filterButton" :class="{ 'is-plain': !value.clicked }">
         {{ value.label }}
       </el-button>
     </el-button-group>
-    <el-button type="primary" @click.native="deleteCompleteTodo()">완료된 일 모두 삭제</el-button>
+    <el-button type="primary" @click.native.prevent="deleteCompleteTodo()">완료된 일 모두 삭제</el-button>
   </div>
 </template>
 
@@ -22,16 +22,19 @@ export default {
       inputValue: '',
       //filtermode: this.$store.getters.filtermode,
       filtermode: [
-        { label: '전체', mode : FILTERMODE.ALL, },
-        { label: '남은 일', mode : FILTERMODE.REMAIN, },
-        { label: '완료된 일', mode : FILTERMODE.COMPLETE, },
+        { label: '전체', mode : FILTERMODE.ALL, clicked: true,},
+        { label: '남은 일', mode : FILTERMODE.REMAIN, clicked: false,},
+        { label: '완료된 일', mode : FILTERMODE.COMPLETE, clicked: false,},
         ],
+      lastfocus: null,
+      classname: '',
     }
   },
   methods: {
-    filterButtonClick(value){
+    filterButtonClick(value, index){
       this.$store.commit(value.mode);
-      debugger;
+      this.filtermode.forEach( x => x.clicked = false )
+      this.filtermode[index].clicked = true;
     },
     deleteCompleteTodo() {
       this.$store.commit('deleteCompleteTodo');
