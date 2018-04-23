1b45e88e9309c385497798e515e2163b1212a784
guegit <ctbcban@gmail.com>
2018-04-23 09:21:09 +0900

    EditCode 필터버튼을 v-for를 이용해서 통일화

--- vuejs/HW/03.Todo/src/views/todo/TodoAppFooter.vue
+++ vuejs/HW/03.Todo/src/views/todo/TodoAppFooter.vue
@@ -1,33 +1,37 @@
 <template>
   <div :class="$style.div">
     <el-button-group>
-      <el-button type="primary" autofocus plain @click.native="filterAll()">전체</el-button>
-      <el-button type="primary" plain @click.native="filterRemain()">남은 일</el-button>
-      <el-button type="primary" plain @click.native="filterComplete()">완료된 일</el-button>
+      <el-button type="primary" plain v-for="(value, index) in filtermode" :key="index" @click.natvie="filterButtonClick(value)">
+        {{ value.label }}
+      </el-button>
     </el-button-group>
     <el-button type="primary" @click.native="deleteCompleteTodo()">완료된 일 모두 삭제</el-button>
   </div>
 </template>
 
 <script>
+const FILTERMODE = {
+  ALL: 'showAllTodo',
+  REMAIN: 'showRemainTodo',
+  COMPLETE: 'showCompleteTodo',
+}
 export default {
   name: 'TodoAppHeader',
   data () {
     return {
       inputValue: '',
-      filtermode: this.$store.getters.filtermode,
+      //filtermode: this.$store.getters.filtermode,
+      filtermode: [
+        { label: '전체', mode : FILTERMODE.ALL, },
+        { label: '남은 일', mode : FILTERMODE.REMAIN, },
+        { label: '완료된 일', mode : FILTERMODE.COMPLETE, },
+        ],
     }
   },
   methods: {
-    filterAll() {
-      this.$store.commit('showAllTodo');
-    },
-    filterRemain() {
-      this.$store.commit('showRemainTodo');
-      console.log('aa',this.filtermode);
-    },
-    filterComplete() {
-      this.$store.commit('showCompleteTodo');
+    filterButtonClick(value){
+      this.$store.commit(value.mode);
+      debugger;
     },
     deleteCompleteTodo() {
       this.$store.commit('deleteCompleteTodo');
