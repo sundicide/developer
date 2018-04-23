50ea6a3824c3c6b018106faabdf3b6cf6efd3bac
guegit <ctbcban@gmail.com>
2018-04-23 15:16:44 +0900

    AddCode 필터모드 상수 추가

--- vuejs/HW/03.Todo/src/constant.js
+++ vuejs/HW/03.Todo/src/constant.js
@@ -2,4 +2,9 @@ export default {
   FETCH_TODOLIST : "fetchTodolist",
   ADD_TODOLIST: "addTodolist",
   DELETE_TODOLIST: "deleteTodolist",
+  FILTER_MODE : {
+    ALL: 'showAllTodo',
+    REMAIN: 'showRemainTodo',
+    COMPLETE: 'showCompleteTodo',
+  },
 }
